f870e31f6f2031fe7d0ebc3eb0275d6f9134d684
Yeye Pranata <yeye@marketbiz.net>
2025-06-17 13:43:10 +0800

    set length mysql

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Set the default string length for database schema to avoid issues with MySQL
+        Schema::defaultStringLength(191);
     }
 }
